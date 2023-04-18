d363ccf99591d631706426478286deade5234339
Dylan Hunt <dylan@imperium42.com>
2023-04-18 17:43:25 +0800

    fix(ps1): Impr log/var clarity

--- Utils/RunWinServerClients.ps1
+++ Utils/RunWinServerClients.ps1
@@ -1,5 +1,7 @@
 ##################################################################################
-# Arg list: https://docs.unity3d.com/Manual/PlayerCommandLineArguments.html 
+# ABOUT: Creates 1 WinServer (with gfx so it's easy to close) + x WinClients.
+##################################################################################
+# ARG LIST: https://docs.unity3d.com/Manual/PlayerCommandLineArguments.html 
 # -batchmode is useful, but it's hard to know when to -quit (or need to end task)
 # -nographics (combined with -batchmode) may improve startup time?
 ##################################################################################
@@ -8,12 +10,12 @@ $PathToExe = "../src/Build-Client/Hathora-Unity.exe"
 $NumClients = Read-Host -Prompt 'Num Clients to Create'
 
 Write-Host ""
-Write-Host "Creating 1 server @ '$PathToExe' ..."
+Write-Host "Creating 1 WinServer @ '$PathToExe' ..."
 & $PathToExe -mode server -single-instance -logfile logs/log-server.txt -memo "Server" 
 Write-Host "Done."
 
 Write-Host ""
-Write-Host "Preparing to create $NumClients client(s):"
+Write-Host "Preparing to create $NumClients WinClient(s):"
 for ($i = 0; $i -le $NumClients)
 {
 	Write-Host "Creating client$i ..."
