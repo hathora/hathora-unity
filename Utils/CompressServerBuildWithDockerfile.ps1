8601119adf4a0d73a1bfef85cb6ff2e9ce5c9ce2
Dylan Hunt <dylan@imperium42.com>
2023-04-19 14:43:35 +0800

    feat(util): CompressServerBuildWIthDockerFile.ps1

---
+++ Utils/CompressServerBuildWithDockerfile.ps1
@@ -0,0 +1,36 @@
+# Define paths
+$sourceDir = "../src/"
+$serverBuildDir = "$sourceDir/Build-Server/"
+$dockerfilePath = "$sourceDir/Dockerfile"
+$archiveName = "Build-Server.tar"
+$compressedArchiveName = "Build-Server.tar.gz"
+$outputPath = "./uploadToHathora/"
+$tempPath = "./_uploadToHathora/"
+
+# Ensure the output path and temporary path exist
+Write-Host "Checking and creating output and temporary directories..."
+if (!(Test-Path -Path $outputPath)) {
+    New-Item -ItemType Directory -Path $outputPath
+}
+if (!(Test-Path -Path $tempPath)) {
+    New-Item -ItemType Directory -Path $tempPath
+}
+
+# Create a tar archive in the temporary path
+Write-Host "Creating tar archive..."
+7z.exe a -ttar "$tempPath$archiveName" $serverBuildDir $dockerfilePath
+
+# Compress the tar archive in the temporary path
+Write-Host "Compressing tar archive..."
+7z.exe a -tgzip "$tempPath$compressedArchiveName" "$tempPath$archiveName"
+
+# Remove the intermediate tar file in the temporary path
+Write-Host "Removing intermediate tar file..."
+Remove-Item "$tempPath$archiveName"
+
+# Move the final tarball from the temporary path to the output path
+Write-Host "Moving final tarball to the output path..."
+Move-Item -Path "$tempPath$compressedArchiveName" -Destination $outputPath
+
+Write-Output ""
+Write-Output "Done - Ready to upload to Hathora @ '$outputPath'"
