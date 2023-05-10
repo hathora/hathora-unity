e1c6fc2ca204cf690c3261777001a82f8a93a40c
Dylan Hunt <dylan@imperium42.com>
2023-05-10 16:50:01 +0800

    feat: HathoraServerApiBase

---
+++ src/Assets/Hathora/Scripts/Utils/Editor/ApiWrapper/HathoraServerApiBase.cs
@@ -0,0 +1,32 @@
+// Created by dylan@hathora.dev
+
+using Hathora.Cloud.Sdk.Client;
+using Hathora.Scripts.Net.Server;
+using UnityEngine;
+
+namespace Hathora.Scripts.Utils.Editor
+{
+    /// <summary>
+    /// This allows the API to view UserConfig (eg: AppId), set session and auth tokens.
+    /// Both Client and Server APIs can inherit from this.
+    /// </summary>
+    public abstract class HathoraServerApiBase : MonoBehaviour
+    {
+        protected Configuration hathoraSdkConfig { get; private set; }
+        protected HathoraServerConfig hathoraServerConfig { get; private set; }
+
+
+        /// <summary>
+        /// Init anytime. Server calls use Dev token.
+        /// </summary>
+        /// <param name="_hathoraSdkConfig">SDK config that we pass to Hathora API calls</param>
+        /// <param name="_hathoraServerConfig">Find via Unity editor top menu: Hathora >> Find Configs</param>
+        public virtual void Init(
+            Configuration _hathoraSdkConfig, 
+            HathoraServerConfig _hathoraServerConfig)
+        {
+            this.hathoraSdkConfig = _hathoraSdkConfig;
+            this.hathoraServerConfig = _hathoraServerConfig;
+        }
+    }
+}
