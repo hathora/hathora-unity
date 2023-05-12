5b81bb9fc2b2ff04a5448f7126c0071834f49909
Dylan Hunt <dylan@imperium42.com>
2023-05-12 14:43:16 +0800

    feat: Config finder new button, lbl

--- src/Assets/Hathora/Scripts/SdkWrapper/Editor/HathoraServerConfigFinder.cs
+++ src/Assets/Hathora/Scripts/SdkWrapper/Editor/HathoraServerConfigFinder.cs
@@ -14,11 +14,28 @@ namespace Hathora.Scripts.SdkWrapper.Editor
     {
         private const string ShowOnStartupKey = "HathoraServerConfigFinder.ShowOnStartup";
         private static List<NetHathoraConfig> serverConfigs;
-        private Vector2 scrollPos;
+        private static Vector2 scrollPos;
+        private static GUIStyle richTxtLabelStyle;
 
+        
+        #region Init
+        /// <summary>Just run once on startup</summary>
         static HathoraServerConfigFinder()
         {
             EditorApplication.delayCall += ShowWindowOnStartup;
+        }
+        
+        /// <summary>
+        /// As soon as we see the window, we find all the configs and select the latest (if any).
+        /// </summary>
+        private void OnEnable()
+        {
+            findAllHathoraServerConfigs();
+        }
+        
+        private static void InitStyles()
+        {
+            richTxtLabelStyle ??= new GUIStyle(EditorStyles.label) { richText = true };
         }
 
         private static void ShowWindowOnStartup()
@@ -38,6 +55,8 @@ namespace Hathora.Scripts.SdkWrapper.Editor
         {
             EditorPrefs.SetBool(ShowOnStartupKey, false);
         }
+        #endregion // Init
+        
 
         [MenuItem("Hathora/Find Configs _%#h", priority = -1000)] // Ctrl + Shift + H
         public static void ShowWindow()
@@ -64,22 +83,46 @@ namespace Hathora.Scripts.SdkWrapper.Editor
             return EditorPrefs.GetBool(ShowOnStartupKey, true);
         }
 
-        private void OnEnable()
-        {
-            findAllHathoraServerConfigs();
-        }
-
         private void OnGUI()
         {
+            InitStyles();
             HathoraEditorUtils.InsertBanner();
-
             EditorGUILayout.Space(10);
-            scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+
+            insertDescrLbl();
+            EditorGUILayout.Space(10);
             
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+    
             // Check if any serverConfigs are null with LINQ: If they are, findAllHathoraServerConfigs() to refresh cache.
             if (serverConfigs != null && serverConfigs.Any(config => config == null))
                 findAllHathoraServerConfigs();
 
+            insertBtnForEachConfigFound();
+            insertNewConfigBtn();
+            
+            EditorGUILayout.Space(5);
+        }
+
+        private void insertDescrLbl()
+        {
+            string headerColor = HathoraEditorUtils.HATHORA_GREEN_HEX;
+            string lblContent = serverConfigs?.Count > 0
+                ? $"<b><color={headerColor}>Choose a Hathora Config:</color></b>"
+                : $"<color={headerColor}>Create a new Hathora config to get started!</color>";
+            
+            GUILayout.Label(lblContent, richTxtLabelStyle);
+        }
+
+        private void insertNewConfigBtn()
+        {
+            // Add the "New Config" button
+            if (GUILayout.Button("New Config", GUILayout.Height(30)))
+                createAndSelectNewConfig();
+        }
+
+        private void insertBtnForEachConfigFound()
+        {
             foreach (NetHathoraConfig config in serverConfigs)
             {
                 EditorGUILayout.BeginHorizontal();
@@ -92,9 +135,32 @@ namespace Hathora.Scripts.SdkWrapper.Editor
             }
 
             EditorGUILayout.EndScrollView();
+        }
 
-            if (serverConfigs.Count == 1)
-                Close();
+        private void createAndSelectNewConfig()
+        {
+            string basePath = "Assets/Hathora";
+            string fullPath = $"{basePath}/NetHathoraConfig.asset";
+
+            // Create Hathora directory if it doesn't exist
+            if (!AssetDatabase.IsValidFolder(basePath))
+                AssetDatabase.CreateFolder("Assets", "Hathora");
+
+            // Create a new instance of NetHathoraConfig
+            NetHathoraConfig newConfig = ScriptableObject.CreateInstance<NetHathoraConfig>();
+
+            // Save the new instance as an asset
+            AssetDatabase.CreateAsset(newConfig, AssetDatabase.GenerateUniqueAssetPath(fullPath));
+
+            // Refresh the AssetDatabase to ensure the new asset is properly saved and recognized
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            // Select the newly created NetHathoraConfig in the Project tab
+            selectHathoraServerConfig(newConfig);
+
+            // Refresh the list of configs
+            findAllHathoraServerConfigs();        
         }
 
         private void findAllHathoraServerConfigs()
