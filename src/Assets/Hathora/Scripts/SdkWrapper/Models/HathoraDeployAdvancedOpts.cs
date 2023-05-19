11f7a59ef3257035a84b372dd9e65b7c914eddc1
Dylan Hunt <dylan@imperium42.com>
2023-05-19 13:12:41 +0800

    feat: Get extra port containers + validations

--- src/Assets/Hathora/Scripts/SdkWrapper/Models/HathoraDeployAdvancedOpts.cs
+++ src/Assets/Hathora/Scripts/SdkWrapper/Models/HathoraDeployAdvancedOpts.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using Hathora.Cloud.Sdk.Model;
 using Hathora.Scripts.Net.Common.Models;
+using Hathora.Scripts.Utils.Extensions;
 using UnityEngine;
 
 namespace Hathora.Scripts.SdkWrapper.Models
@@ -41,7 +42,47 @@ namespace Hathora.Scripts.SdkWrapper.Models
             
             
         // Public utils
+
+        /// <summary>Useful for CreateDeploymentAsync() via HathoraDeployOpts.</summary>
+        public bool CheckHasAdditionalContainerPorts() =>
+            !string.IsNullOrEmpty(_extraTransportInfo1?.TransportNickname) ||
+            !string.IsNullOrEmpty(_extraTransportInfo2?.TransportNickname); 
+
+        /// <summary>
+        /// Returns a List of Hathora SDK ContainerPort.
+        /// </summary>
+        /// <returns>Returns an init'd but empty list, if nothing or invalid.</returns>
+        public List<ContainerPort> GetExtraContainerPorts()
+        {
+            if (!CheckHasAdditionalContainerPorts())
+                return null;
             
+            List<ContainerPort> extraContainerPorts = new();
+            
+            if (checkIsValidExtraTransportInfo(_extraTransportInfo1))
+                extraContainerPorts.AddIfNotNull(ParseToSdkContainerToPort(_extraTransportInfo1));
+            
+            if (checkIsValidExtraTransportInfo(_extraTransportInfo2))
+                extraContainerPorts.AddIfNotNull(ParseToSdkContainerToPort(_extraTransportInfo2));
+
+            return extraContainerPorts;
+        }
+
+        private bool checkIsValidExtraTransportInfo(ExtraContainerPortWrapper containerPortWrapper) =>
+            !string.IsNullOrEmpty(containerPortWrapper?.TransportNickname) &&
+            containerPortWrapper.PortNumber != 7777;
+
+        public ContainerPort ParseToSdkContainerToPort(ExtraContainerPortWrapper containerPortWrapper)
+        {
+            if (containerPortWrapper == null)
+                return null;
+            
+            return new ContainerPort(
+                containerPortWrapper.TransportType,
+                containerPortWrapper.PortNumber,
+                containerPortWrapper.GetTransportNickname());
+        }
+        
         /// <summary>
         /// Explicit typings for FindNestedProperty() calls
         /// </summary>
