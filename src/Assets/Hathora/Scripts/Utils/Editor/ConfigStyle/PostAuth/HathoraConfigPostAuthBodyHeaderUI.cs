d61b879a7c1558c31d7637a4f2e28b0d49e3feb2
Dylan Hunt <dylan@imperium42.com>
2023-06-01 12:42:15 +0800

    chore: Rm pkgs-cinema, collab, timeline

--- src/Assets/Hathora/Scripts/Utils/Editor/ConfigStyle/PostAuth/HathoraConfigPostAuthBodyHeaderUI.cs
+++ src/Assets/Hathora/Scripts/Utils/Editor/ConfigStyle/PostAuth/HathoraConfigPostAuthBodyHeaderUI.cs
@@ -2,14 +2,12 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Hathora.Cloud.Sdk.Model;
 using Hathora.Scripts.Net.Common;
 using Hathora.Scripts.SdkWrapper.Editor;
 using Hathora.Scripts.SdkWrapper.Editor.ApiWrapper;
-using PlasticGui.WorkspaceWindow.Topbar;
 using UnityEditor;
 using UnityEngine;
 
