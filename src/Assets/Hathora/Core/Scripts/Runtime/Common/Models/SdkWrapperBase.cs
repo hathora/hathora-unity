718b5733a67cbf2aafd8a94b5debaea33e2248bf
Dylan Hunt <dylan@imperium42.com>
2023-06-06 17:30:26 +0800

    feat: SdkWrapperBase

---
+++ src/Assets/Hathora/Core/Scripts/Runtime/Common/Models/SdkWrapperBase.cs
@@ -0,0 +1,25 @@
+// Created by dylan@hathora.dev
+
+using System;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Hathora.Core.Scripts.Runtime.Common.Models
+{
+    /// <summary>
+    /// Generally, most (if not all) SDK models include AppId.
+    /// </summary>
+    [Serializable]
+    public class SdkWrapperBase : MonoBehaviour
+    {
+        [SerializeField, JsonProperty("appId")]
+        private string _appId;
+        public string AppId
+        {
+            get => _appId;
+            set => _appId = value;
+        }
+        
+        
+    }
+}
