9a6b286ed34907c00fc3534fa34797554b336bac
Dylan Hunt <dylan@imperium42.com>
2023-06-16 17:57:57 +0800

    fix(fishnet): Prevent conflicts +ns

--- src/Assets/Hathora/Demos/1-FishNetDemo/FishNet/TutorialInfo/Scripts/Readme.cs
+++ src/Assets/Hathora/Demos/1-FishNetDemo/FishNet/TutorialInfo/Scripts/Readme.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using UnityEngine;
 
-public class Readme : ScriptableObject {
-	public Texture2D icon;
-	public string title;
-	public Section[] sections;
-	public bool loadedLayout;
+// (!) Added ns to prevent conflicts in 2D Space Shooter, and probably others
+namespace Hathora.Demos._1_FishNetDemo.FishNet.TutorialInfo.Scripts
+{
+	public class Readme : ScriptableObject {
+		public Texture2D icon;
+		public string title;
+		public Section[] sections;
+		public bool loadedLayout;
 	
-	[Serializable]
-	public class Section {
-		public string heading, text, linkText, url;
+		[Serializable]
+		public class Section {
+			public string heading, text, linkText, url;
+		}
 	}
 }
