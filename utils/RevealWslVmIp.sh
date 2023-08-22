a3ffd709b35eb8ad62e7435f88c7905bc4f34cf8
Dylan Hunt <dylan@imperium42.com>
2023-08-22 14:46:59 +0800

    fix: Shell script line ending fixes

--- utils/RevealWslVmIp.sh
+++ utils/RevealWslVmIp.sh
@@ -3,5 +3,6 @@
 # If `localhost` doesn't work for you when testing a localhost`
 #   Linux server via WSL2 in Windows, then use this script
 #   to get the local IP of the WSL VM.
+# v1.0.1
 ################################################################
 ip addr show eth0 | grep 'inet ' | awk '{ print $2; }' | sed 's/\/.*$//'
