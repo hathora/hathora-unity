e46206fea81e57407c01f3c7cbdf4a78c1affc2b
Dylan Hunt <dylan@imperium42.com>
2023-09-26 10:41:12 +0800

    chore: Cleanup unused

--- src/Assets/Hathora/Core/Scripts/Editor/Server/HathoraServerConfigFinder.cs
+++ src/Assets/Hathora/Core/Scripts/Editor/Server/HathoraServerConfigFinder.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Hathora.Core.Scripts.Runtime.Server;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace Hathora.Core.Scripts.Editor.Server
@@ -12,7 +11,6 @@ namespace Hathora.Core.Scripts.Editor.Server
     public class HathoraServerConfigFinder : EditorWindow
     {
         private const string LAST_FOCUSED_SERVER_CONFIG_PATH = "HathoraServerConfigFinder.LastFocusedServerConfigPath";
-        private static List<HathoraServerConfig> dynamicConfigs = new();
         
         
         #region Menu Items
