4d5250969bffa6036c7f02a9090a969c7d24f3c5
Dylan Hunt <dylan@imperium42.com>
2023-09-27 17:33:50 +0800

    feat: BoilerStateMgr !relies on demo

--- src/Assets/Hathora/Boilerplate/Scripts/BoilerStateMgr.cs
+++ src/Assets/Hathora/Boilerplate/Scripts/BoilerStateMgr.cs
@@ -1,42 +1,62 @@
 // Created by dylan@hathora.dev
 
-using Hathora.Demos.Shared.Scripts.Common;
+using System;
+using Hathora.Core.Scripts.Runtime.Client;
+using HathoraCloud.Models.Shared;
 using UnityEngine;
 
 namespace HathoraBoiler.Scripts
 {
     /// <summary>
     /// Acts as the liason between NetworkManager and HathoraClientMgr.
-    /// - Base contains funcs like: StartServer, StartClient, StartHost.
-    /// - Base contains events like: OnClientStarted, OnClientStopped.
-    /// - Base tracks `ClientState` like: Stopped, Starting, Started.
+    /// - Contains funcs like: StartServer, StartClient, StartHost.
+    /// - Contains events like: OnClientStarted, OnClientStopped.
+    /// - Tracks `ClientState` like: Stopped, Starting, Started.
     /// </summary>
-    public class BoilerStateMgr : NetworkMgrStateTracker
+    public class BoilerStateMgr : MonoBehaviour
     {
-        #region vars
-        /// <summary>
-        /// `New` keyword overrides base Singleton when accessing child directly.
-        /// </summary>
-        public new static BoilerStateMgr Singleton { get; private set; }
+        #region Core vars
+        [SerializeField, Tooltip("Log all client state changes")]
+        private bool verboseLogs = true;
+        public bool VerboseLogs => verboseLogs;
+
+        /// <summary>When a connection stops, errs out or we get d/c'd, what should we show to clients?</summary>
+        protected const string CONNECTION_STOPPED_FRIENDLY_STR = "Connection Stopped";
+        
+        public static BoilerStateMgr Singleton { get; private set; }
         
         // TODO: You may want to add a shortcut to your NetworkManager instance, or Transport instance(s).  
-        #endregion // vars
+        #endregion // Core vars
 
+        
+        #region Client Events
+        /// <summary>Event triggers when a NetworkManager client is attempting to connect.</summary>
+        public static event Action OnClientConnectingEvent;
+        
+        /// <summary>Event triggers when a NetworkManager client is connected and starting (but not yet started).</summary>
+        public static event Action OnClientStartingEvent;
+        
+        /// <summary>Event triggers when a NetworkManager client started.</summary>
+        /// <returns>roomId, friendlyRegion</returns>
+        public static event Action OnClientStartedEvent;
+
+        /// <summary>Event triggers when a NetworkManager client stopped (or disconnected).</summary>
+        public static Action OnClientStoppedEvent;
+        
+        /// <summary>Event triggers when a NetworkManager client failed to start (with UI-friendly err).</summary>
+        /// <returns>friendlyReason</returns>
+        public static event Action<string> OnStartClientFailEvent;
+        #endregion // Client Events
+        
         
         #region Init
         /// <summary>Set Singleton instance</summary>
-        protected override void Awake()
-        {
-            base.Awake(); // Sets base singleton
+        private void Awake() =>
             setSingleton();
-        }
 
         /// <summary>Subscribe to NetworkManager Client state changes</summary>
-        protected override void Start()
-        {
-            base.Start();
+        private void Start() =>
             subToNetworkManagerStateEvents();
-        }
 
         /// <summary>Catch events like OnNetworkStart and handle them below.</summary>
         private void subToNetworkManagerStateEvents()
@@ -61,8 +81,125 @@ namespace HathoraBoiler.Scripts
             }
 
             Singleton = this;
-        }
+        }        
         #endregion // Init
+        
+        
+        #region Client State
+        /// <summary>Client state breadcrumbs</summary>
+        public enum ClientTrackedState
+        {
+            /// <summary>Disconnected or failed to connect</summary>
+            Stopped,
+            
+            /// <summary>We're just about to attempt to connect</summary>
+            Connecting,
+
+            /// <summary>Connected, but not yet started</summary>
+            Starting,
+
+            /// <summary>Connected and started</summary>
+            Started,
+        }
+        
+        /// <summary>What's our status?</summary>
+        protected ClientTrackedState ClientState { get; set; }
+
+        protected virtual void OnClientConnecting()
+        {
+            if (verboseLogs)
+                Debug.Log($"[{nameof(BoilerStateMgr)}] {nameof(OnClientConnecting)}");
+            
+            ClientState = ClientTrackedState.Connecting;
+            OnClientConnectingEvent?.Invoke(); 
+        }
+        
+        /// <summary>
+        /// We are already connected: We are starting, but not yet ready (!started).
+        /// OnClientConnectionState() success callback when state == Started. Before this:
+        /// - We already called StartClient() || StartClientToLastCachedRoom()
+        /// - IsConnectingAsClient == true
+        /// </summary>
+        protected virtual void OnClientStarting()
+        {
+            if (verboseLogs)
+                Debug.Log($"[{nameof(BoilerStateMgr)}] {nameof(OnClientStarting)}");
+            
+            ClientState = ClientTrackedState.Starting;
+            OnClientStartingEvent?.Invoke();
+        }
+
+        /// <summary>We just started and can now run net code</summary>
+        protected virtual void OnClientStarted()
+        {
+            if (verboseLogs)
+                Debug.Log($"[{nameof(BoilerStateMgr)}] {nameof(OnClientStarted)}");
+
+            ClientState = ClientTrackedState.Started;
+            OnClientStartedEvent?.Invoke();
+        }
+
+        /// <summary>We were disconnected from net code</summary>
+        protected virtual void OnClientStopped()
+        {
+            if (verboseLogs)
+                Debug.Log($"[{nameof(BoilerStateMgr)}] {nameof(OnClientStopped)}");
+            
+            ClientState = ClientTrackedState.Stopped;
+            OnClientStoppedEvent?.Invoke();
+        }
+        
+        /// <summary>
+        /// Tried to connect to a Server as a Client, but failed.
+        /// Triggers OnStartClientFailEvent -> OnClientStopped().
+        /// </summary>
+        /// <param name="_friendlyReason"></param>
+        protected virtual void OnStartClientFail(string _friendlyReason)
+        {
+            if (verboseLogs)
+                Debug.Log($"[{nameof(BoilerStateMgr)}.{nameof(OnClientStopped)}] {_friendlyReason}");
+            
+            OnStartClientFailEvent?.Invoke(_friendlyReason);
+            OnClientStopped();
+        }
+        #endregion // Client State
+        
+        
+        #region Hathora
+        /// <summary>
+        /// Get the last queried "host:port" from a Hathora Client session.
+        /// - From `HathoraClientSession.ServerConnectionInfo.ExposedPort`.
+        /// </summary>
+        public string GetHathoraSessionHostPort()
+        {
+            ExposedPort connectInfo = HathoraClientSession.Singleton.ServerConnectionInfo.ExposedPort;
+
+            string hostPort = $"{connectInfo.Host}:{connectInfo.Port}";
+            return hostPort;
+        }
+        #endregion Hathora
+        
+        
+        #region Utils
+        /// <summary>
+        /// This was likely passed in from the UI to override the default
+        /// NetworkManager (often from Standalone Client). Eg:
+        /// "1.proxy.hathora.dev:12345" -> "1.proxy.hathora.dev", 12345
+        /// </summary>
+        /// <param name="_hostPort"></param>
+        /// <returns></returns>
+        protected static (string hostNameOrIp, ushort port) SplitPortFromHostOrIp(string _hostPort)
+        {
+            if (string.IsNullOrEmpty(_hostPort))
+                return default;
+            
+            string[] hostPortArr = _hostPort.Split(':');
+            string hostNameOrIp = hostPortArr[0];
+            ushort port = ushort.Parse(hostPortArr[1]);
+            
+            return (hostNameOrIp, port);
+        }
+        #endregion // Utils
         
         
         #region NetworkManager Server
@@ -156,7 +293,7 @@ namespace HathoraBoiler.Scripts
             //     $"{transport.GetPort()}` via `{transportName}` transport");
             #endregion
             
-            base.OnClientConnecting(); // => callback @ OnClientConected() || OnStartClientFail()
+            OnClientConnecting(); // => callback @ OnClientConected() || OnStartClientFail()
             bool startedConnection = false; // NetworkManager.Singleton.StartClient(); // TODO
             return startedConnection;
         }
